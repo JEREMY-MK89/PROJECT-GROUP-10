932262f3c10d27662ed99242bc7f39e846495d8c
Bakeloh <bakelohnoel@gmail.com>
2023-12-14 23:54:47 +0300

    Adding footer to App.js

--- airbnb-app/src/App.js
+++ airbnb-app/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-////import './App.css';///
-import 'semantic-ui-css/semantic.min.css';
+import React, { useState, useEffect } from "react";
+import './App.css';
+// import 'semantic-ui-css/semantic.min.css';
+import Header from "./components/Header";
+import Listings from './components/Listings';
+import SortIcons from './components/SortIcons';
+import Footer from './components/Footer';
 
 function App() {
+
+  const [allHouses, setAllHouses] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/JEREMY-MK89/Airbnb-Backend-Set-up-two/details")
+      .then(response => response.json())
+      .then(data => setAllHouses(data));
+  }, []);
+
+// Function for handling search change
+  function onSearchChange(search) {
+    if (search !== '') {
+      const searchResult = allHouses.filter((house) => house.Location.toLowerCase().startsWith(search.toLowerCase()))
+      setAllHouses(searchResult)
+    } else {
+      fetch("https://my-json-server.typicode.com/JEREMY-MK89/Airbnb-Backend-Set-up-two/details")
+        .then(response => response.json())
+        .then(data => setAllHouses(data))
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Airbnb
-        </a>
-      </header>
+    <div className="App ml-2 my-1">
+      <Header onSearch={onSearchChange} />
+      <SortIcons />
+      <Listings houses={allHouses} />
+      <Footer />
     </div>
   );
 }
