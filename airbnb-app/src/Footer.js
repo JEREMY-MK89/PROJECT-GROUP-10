3c09ba364bec6e0a64090d0dbeefd563ed9b1f51
Bakeloh <bakelohnoel@gmail.com>
2023-12-14 21:20:56 +0300

    Fotter Details

---
+++ airbnb-app/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Group X </p>
+        <p>Contact: groupX@moringa.com</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
