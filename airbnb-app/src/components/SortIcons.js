016bb2f75e980e572870dbc0a2d8a2820251813f
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-17 21:27:56 +0300

    Implemented the function for sorting the houses using the icons on the SortIcons component.

--- airbnb-app/src/components/SortIcons.js
+++ airbnb-app/src/components/SortIcons.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SortIcons() {
+function SortIcons({ sortByIcon }) {
     return (
         <div className="flex items-center border border-black p-1">
             <img src="/new-house-svgrepo-com.svg" alt='sort' className="h-10 w-10 mr-4" />
