79e41e0f7e7eabc9219244747402ddc19fb3916d
JEREMY-MK89 <144157129+JEREMY-MK89@users.noreply.github.com>
2023-12-18 11:16:39 +0300

    Update App.js to include search functionality

--- airbnb-app/src/App.js
+++ airbnb-app/src/App.js
@@ -1,41 +1,57 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-// import 'semantic-ui-css/semantic.min.css';
 import Header from "./components/Header";
 import Listings from './components/Listings';
 import SortIcons from './components/SortIcons';
-import Footer from './components/Footer';
+import Footer from "./Footer";
 
 function App() {
-
   const [allHouses, setAllHouses] = useState([]);
-  const [setFIlteredHouses, setFIlteredHouses] = useState([])
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch("https://my-json-server.typicode.com/JEREMY-MK89/Airbnb-Backend-Set-up-two/details")
       .then(response => response.json())
-      .then(data => setAllHouses(data));
+      .then(data => {
+        setAllHouses(data);
+        setFilteredData(data);
+      });
   }, []);
 
-  // Function for handling sort by icon
-  function handleSortByIcon(altText) {
-    if (altText !== 'clear') {
-      const filterByIcon = allHouses.filter((house) => house.category.toLowerCase() === altText.toLowerCase())
-      setAllHouses(filterByIcon)
+  // Function for handling search change
+  function onSearchChange(search) {
+    setSearchTerm(search);
+    filterData(search, allHouses);
+  }
+
+  // Function for handling icon click
+  const handleIconClick = (iconSrc) => {
+    // Filter houses based on the clicked icon source
+    const filtered = allHouses.filter((house) => house.image_url === iconSrc);
+  
+    // Update the filtered data and apply additional filters if needed
+    setFilteredData(filtered);
+    filterData(searchTerm, filtered);
+  };
+  
+
+  // Function to filter data based on search term and icon click
+  function filterData(search, data) {
+    if (search !== '') {
+      const searchResult = data.filter((house) => house.Location.toLowerCase().startsWith(search.toLowerCase()));
+      setFilteredData(searchResult);
     } else {
-      fetch("https://my-json-server.typicode.com/JEREMY-MK89/Airbnb-Backend-Set-up-two/details")
-        .then(response => response.json())
-        .then(data => setAllHouses(data))
+      setFilteredData(data);
     }
   }
 
-
   return (
     <div className="App ml-2 my-1">
-      <Header />
-      <SortIcons sortByIcon={handleSortByIcon} />
-      <Listings houses={allHouses} />
-      <Footer />
+      <Header onSearch={onSearchChange} />
+      <SortIcons onIconClick={handleIconClick} />
+      <Listings houses={filteredData.length > 0 ? filteredData : allHouses} />
+      <Footer/>
     </div>
   );
 }
